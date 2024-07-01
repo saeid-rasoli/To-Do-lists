bb487312011cd3f7b5faa5e82b157424c095a0c8
saeid-rasoli <saeidrasoli1212@gmail.com>
2024-07-01 22:43:15 +0330

    adding templates and new class based views

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, {})
+    path('', views.ListListView.as_view(), name='index'),
+    path('list/<int:list_id>/', views.ItemListView.as_view(), name='list')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView
-from .models import TodoList
+from .models import TodoList, TodoItem
 
 # Create your views here.
 class ListListView(ListView):
-    model = ListView
+    model = TodoList
     template_name = 'todo_app/index.html'
+
+class ItemListView(ListView):
+    model = TodoItem
+    tempalte_name = 'todo_app/todo_list.html'
+
+    def get_queryset(self):
+        return TodoItem.objects.filter(todo_list_id=self.kwargs['list_id'])
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = TodoList.objects.get(id=self.kwargs['list_id'])
+        return context
+    
+
+    
